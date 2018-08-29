1c9e244e2d63b92316b66553a7f78c28b7b716f4
Corey Noel <corey.g.noel@gmail.com>
2018-08-29 00:50:06 -0400

    added layout.erb file, preparing for sinatra message flashing

--- public/scripts/remote-controller.js
+++ public/scripts/remote-controller.js
@@ -1,13 +1,13 @@
-// when the DOM finishes loading
-// overrides the
-$(document).ready(function() {
-  $("form[name='update']").on('submit', function(e) {
-    console.log("sending form");
-    send_update();
-    e.preventDefault();
-  });
+// runs when the DOM finishes loading
+// overrides the natural function of the form
+// instead makes it run the send_update function
+$("form[name='update']").on('submit', function(e) {
+  send_update();
+  e.preventDefault();
 });
 
+// sends an update to the server
+// server will update the .json file
 function send_update() {
   vals = $("form[name='update']").serializeArray().reduce(function(obj, item) {
     obj[item.name] = item.value;
--- server.rb
+++ server.rb
@@ -1,10 +1,20 @@
 require 'sinatra'
+require 'sinatra/flash'
+require 'json'
 
-get '/' do
-  erb :test
+enable :sessions
+
+filename = "streamcontrol.json"
+
+get '/control' do
+  erb :index
 end
 
 post '/update' do
-  puts request.body.read
+  vals = JSON.parse(request.body.read)
+  vals[:timestamp] = Time.new.to_i
+  File.open(filename, "w") do |f|
+    f.write(vals.to_json)
+  end
+  200
 end
-
